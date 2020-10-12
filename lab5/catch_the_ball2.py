8f2b886824e8f9f5352b28e87d3784ba0bff628c
Titov Ilya <titov.iiu@phystech.edu>
2020-10-12 13:19:08 +0300

    Добавил движение нескольких шаров

---
+++ lab5/catch_the_ball2.py
@@ -0,0 +1,91 @@
+import pygame
+from pygame.draw import *
+from random import randint
+pygame.init()
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+X = 0
+Y = 1
+R = 2
+COLOR = 3
+VX = 4
+VY = 5
+WIDTH = 1200
+HEIGHT= 1200
+
+FPS = 20
+screen = pygame.display.set_mode((HEIGHT, WIDTH))
+
+number = 1000
+balls = [0]*number
+def draw(ball):
+	circle(screen,ball[COLOR],(ball[X],ball[Y]),ball[R])
+
+
+def new_ball():
+	'''рисует новый шарик '''
+
+	x = randint(100, 1100)
+	y = randint(100, 900)
+	vx = randint(-10, 10)
+	vy = randint(-10, 10)
+	r = randint(10, 100)
+	color = COLORS[randint(0, 5)]
+	circle(screen, color, (x, y), r)
+	ball = [0]*6
+	ball[X] = x
+	ball[Y] = y
+	ball[VX] = vx
+	ball[VY] = vy
+	ball[R] = r
+	ball[COLOR] = color
+	return ball
+
+def move_ball(ball):
+	
+	x = ball[X] 
+	y = ball[Y] 
+	vx = ball[VX]
+	vy = ball[VY]
+	x += vx
+	y += vy
+
+	ball[X] = x
+	ball[Y] = y
+
+	return ball
+	
+for i, ball in enumerate(balls):
+	balls[i] = new_ball()
+				
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+	clock.tick(FPS)
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			finished = True
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+				for i, ball in enumerate(balls):
+					balls[i] = new_ball()
+				print('Click!')
+	
+	for ball in balls:
+		ball = move_ball(ball)
+		draw(ball)
+
+
+	pygame.display.update()
+	screen.fill(BLACK)
+
+pygame.quit()
